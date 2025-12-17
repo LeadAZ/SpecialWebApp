51cdc43a195f30f0050d099eb1e4e64517228c1b
meliora <52936882+PikaWizard@users.noreply.github.com>
2025-12-17 20:15:00 +0000

    12/17/25 test 4.3 - button with js function is born and hopefully is not a malformed baby

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-Notification.requestPermission()
+// SETUP //
+
+const createstudentbutton = document.getElementById('createstudentbutton')
+
+// FUNCTIONS //
+
+function createStudent() {
+
+    Notification.requestPermission();
+    console.log("requested permission from user for notification access");
+
+    const button = document.getElementById('createstudentbutton');
+    const inputText = document.getElementById('createstudenttext');
+    const outputText = document.getElementById('createstudentoutputtext');
+
+    if (inputText.trim() != "") {
+        outputText.textContent = 'You have added student ' + inputText + '.';
+    } else {
+        outputText.textContent = 'Please enter a name.';
+    }
+    
+}
+
+// RUNTIME //
+
+createstudentbutton.addEventListener('click', createStudent);
