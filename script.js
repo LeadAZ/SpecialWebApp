9f7a9b6984f333ed1948014ab60213be355098fa
meliora <52936882+PikaWizard@users.noreply.github.com>
2025-12-17 21:00:46 +0000

    12/17/25 test 4.67 - we are making another malformed baby

--- script.js
+++ script.js
@@ -23,6 +23,31 @@ function createStudent() {
     
 }
 
+function addCivPoints() {
+
+    const button = document.getElementById('addcivpointsbutton');
+    const inputText_amount = document.getElementById('addcivpointstext_amount');
+    const inputText_userID = document.getElementById('addcivpointstext_student');
+    const outputText = document.getElementById('addcivpointsoutputtext');
+
+    const inputValue_amount = inputText_amount.value;
+    const inputValue_userID = inputText_userID.value;
+    
+    if (Number(inputValue_amount) == NaN) {
+        outputText.textContent = "Message: Please input a valid amount.";
+        return;
+    }
+
+    if (Number(inputText_userID) == NaN) {
+        outputText.textContent = "Message: Please input a valid user ID.";
+    }
+
+    // whatever request you do to add civ points after
+
+    outputText.textContent = "Message: You have added " + inputValue_amount + " to user " + inputText_userID + ".";
+
+}
+
 // RUNTIME //
 
 createstudentbutton.addEventListener('click', createStudent);
