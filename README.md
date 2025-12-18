fee03164e2aebf88762f193d26d4f5a9d05c6dd6
BrokenLatchhook <143334871+BrokenLatchhook@users.noreply.github.com>
2025-12-18 01:16:51 +0000

    12/17/25 test 5.0 with also cleaning up stuff no malformed babies here hopefully

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SpecialWebApp
 Personal Attempt at making a web app for learning purposes.
-routes:
+# routes:
 /createstudent
     Creates a new student entry. Requires Authentication.
     Expects JSON body: {"id": "student_id", "name": "student_name"}
@@ -28,3 +28,8 @@ Readonly, does not require staff authentication.
 what it will return for this route only: (this is not the beautiful version so it is harder to read)
 {"civ_points":-4453,"id":"456789","log":[{"change":-7616,"reason":"1oz Rand Gold Bar (.9999 Purity)","timestamp":"2025-09-20T14:21:40.533794+00:00"},{"change":21,"reason":"Funny number","timestamp":"2025-09-20T14:22:07.480530+00:00"},{"change":11,"reason":"Active Service","timestamp":"2025-09-24T19:10:09.750261+00:00"},{"change":10,"reason":"Jessup","timestamp":"2025-09-24T19:50:35.664070+00:00"},{"change":100,"reason":"Jessup1","timestamp":"2025-09-24T19:54:03.859473+00:00"},{"change":21,"reason":"Test","timestamp":"2025-09-30T18:48:02.645028+00:00"},{"change":3000,"reason":"Test","timestamp":"2025-12-01T17:57:44.756146+00:00"}],"name":"Cool Person"}
 
+# POST, GET, PUT, AND DELETE MEANING
+so POST just posts it for the backend to see
+PUT adds stuff
+DELETE deletes stuff
+wow
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // ;) //
 console.log("this malformed baby NOT is a faliure, gg");
+console.log("gg indeed.");
 
 // SETUP //
 
@@ -49,8 +50,36 @@ function addCivPoints() {
     }
 
     // whatever request you do to add civ points after
-    outputText.textContent = "Message: You have added " + inputValue_amount + " to user " + inputValue_userID + "."; console.log("success")
-
+    //got it
+    //yknow i just realized we should move this to the bottom maybe because it hasn't actually succeeded yet. i'll comment it out for now and move it.
+    //outputText.textContent = "Message: You have added " + inputValue_amount + " to user " + inputValue_userID + "."; console.log("success")
+    // "fetching" the information to the backend
+    fetch('', {
+        //you problably know what these mean
+        method: 'POST',
+        //there are like around 4 HTTP official requests, like POST, PUT, GET, and DELETE. I'll make a more detailed guide in the README.
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        //I never really dug into why this content-type is like this but it's always required
+        body: JSON.stringify({
+            //the body is the data to send, and we need to convert it into a json string or else our backend doesn't like us anymore
+            //All you did before was just check if the strings can be converted to valid numbers.
+            //We actually have to do that work now.
+            id: Number(inputValue_userID), //these both are pretty self-explanatory
+            points: Number(inputValue_amount),
+            //we forgot to add the reason so i just made it non-adjustable for now
+            //you can edit that later if you want
+            reason: "this will NOT be a malformed baby hopefully" //You can change this to anything.
+        })
+    })
+    //final real succeed line
+    //I just realized we don't really have any way to say that it failed from the backend...
+    //fine for now
+    //You can add that if you really want to
+    //I know we shouldn't go without error handling but this is just a project.
+    //If I really need to and this fails I will add it and take what the backend HTTP code was.
+    outputText.textContent = "Message: You have added " + inputValue_amount + " to user " + inputValue_userID + "."; console.log("success yay")
 }
 
 // RUNTIME //
